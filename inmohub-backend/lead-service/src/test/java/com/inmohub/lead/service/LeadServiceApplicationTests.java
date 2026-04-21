b072468cf406a3aeb97b2a44b932edc7d0129be2
Juacole <jvacotunjar@gmail.com>
2026-04-21 17:13:49 +0200

    feat: creación de lead-service y config inicial

---
+++ inmohub-backend/lead-service/src/test/java/com/inmohub/lead/service/LeadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inmohub.lead.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeadServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
