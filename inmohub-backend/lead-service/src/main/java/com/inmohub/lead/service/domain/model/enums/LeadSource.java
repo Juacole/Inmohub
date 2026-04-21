ac57471fb5abe2408dffafa8a98ecb33eed850b8
Juacole <jvacotunjar@gmail.com>
2026-04-21 17:19:38 +0200

    feat: agregar enums

---
+++ inmohub-backend/lead-service/src/main/java/com/inmohub/lead/service/domain/model/enums/LeadSource.java
@@ -0,0 +1,8 @@
+package com.inmohub.lead.service.domain.model.enums;
+
+public enum LeadSource {
+    WEB,
+    FSBO,
+    MANUAL,
+    OTHER
+}
---
+++ inmohub-backend/lead-service/src/main/java/com/inmohub/lead/service/domain/model/enums/LeadStatus.java
@@ -0,0 +1,9 @@
+package com.inmohub.lead.service.domain.model.enums;
+
+public enum LeadStatus {
+    NEW,
+    CONTACTED,
+    NEGOTIATION,
+    CLOSED,
+    LOST
+}
