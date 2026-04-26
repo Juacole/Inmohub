c72c44c99c58b5e2c988360062b6f953ffbc0f87
Juacole <jvacotunjar@gmail.com>
2026-04-27 01:21:28 +0200

    feat: agregar configuración inicial de fsbo-ingestor-service

---
+++ inmohub-backend/fsbo-ingestor-service/src/main/java/com/inmohub/fsbo/ingestor/service/FsboIngestorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.inmohub.fsbo.ingestor.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsboIngestorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsboIngestorServiceApplication.class, args);
+	}
+
+}
