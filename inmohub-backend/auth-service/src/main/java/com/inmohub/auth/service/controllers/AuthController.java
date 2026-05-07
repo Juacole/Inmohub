d0ea60b9d41d5f5deefc5359dcd82f11b6e88663
Juacole <jvacotunjar@gmail.com>
2026-05-07 17:25:42 +0200

    feat: agregar preautorización en métodos

--- inmohub-backend/auth-service/src/main/java/com/inmohub/auth/service/controllers/AuthController.java
+++ inmohub-backend/auth-service/src/main/java/com/inmohub/auth/service/controllers/AuthController.java
@@ -12,6 +12,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -29,32 +30,73 @@ import java.util.UUID;
 public class AuthController {
     private final UserService userService;
 
-    @Operation(summary = "Registrar nuevo usuario", description = "Crea un usuario con rol específico (ADMIN, AGENT, CLIENT, OWNER).")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Usuario creado exitosamente",
-                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserDto.class))),
-            @ApiResponse(responseCode = "400", description = "Datos de entrada inválidos", content = @Content),
-            @ApiResponse(responseCode = "409", description = "Conflicto: El email o username ya existen", content = @Content)
-    })
     @PostMapping("/register")
+    @Operation(
+            summary = "Registrar nuevo usuario",
+            description = "Crea un usuario con rol específico (ADMIN, AGENT, CLIENT, OWNER)."
+    )
+    @ApiResponses(
+            value = {
+                @ApiResponse(
+                        responseCode = "200", description = "Usuario creado exitosamente",
+                        content = @Content(
+                                mediaType = "application/json",
+                                schema = @Schema(implementation = UserDto.class)
+                        )
+                ),
+                @ApiResponse(
+                        responseCode = "400",
+                        description = "Datos de entrada inválidos",
+                        content = @Content
+                ),
+                @ApiResponse(
+                        responseCode = "409",
+                        description = "Conflicto: El email o username ya existen",
+                        content = @Content
+                )
+            }
+    )
     public ResponseEntity<UserDto> create(@Valid @RequestBody UserCreateDto createDto) {
         return ResponseEntity.ok(userService.createUser(createDto));
     }
 
-    @Operation(summary = "Listar todos los usuarios", description = "Devuelve un listado completo de los usuarios registrados.")
-    @ApiResponse(responseCode = "200", description = "Operación exitosa")
     @GetMapping("/all")
+    @PreAuthorize("hasAnyRole('ADMIN', 'AGENT')")
+    @Operation(
+            summary = "Listar todos los usuarios",
+            description = "Devuelve un listado completo de los usuarios registrados."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Operación exitosa"
+    )
     public ResponseEntity<List<UserDto>> getAll() {
         return ResponseEntity.ok(userService.getAllUsers());
     }
 
-    @Operation(summary = "Obtener usuario por ID", description = "Busca un usuario específico por su UUID.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Usuario encontrado",
-                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserDto.class))),
-            @ApiResponse(responseCode = "404", description = "Usuario no encontrado", content = @Content)
-    })
     @GetMapping("/search-by-id/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'AGENT')")
+    @Operation(
+            summary = "Obtener usuario por ID",
+            description = "Busca un usuario específico por su UUID."
+    )
+    @ApiResponses(
+            value = {
+                @ApiResponse(
+                        responseCode = "200",
+                        description = "Usuario encontrado",
+                        content = @Content(
+                                mediaType = "application/json",
+                                schema = @Schema(implementation = UserDto.class)
+                        )
+                ),
+                @ApiResponse(
+                        responseCode = "404",
+                        description = "Usuario no encontrado",
+                        content = @Content
+                )
+            }
+    )
     public ResponseEntity<UserDto> getById(@PathVariable(name = "id") UUID id) {
         UserDto user = userService.getById(id);
 
@@ -65,12 +107,24 @@ public class AuthController {
                 .build();
     }
 
-    @Operation(summary = "Verificar existencia por Email", description = "Comprueba si un email ya está registrado en el sistema.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "El email existe"),
-            @ApiResponse(responseCode = "404", description = "El email no existe")
-    })
     @GetMapping("/exists-by-email/{email}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'AGENT')")
+    @Operation(
+            summary = "Verificar existencia por Email",
+            description = "Comprueba si un email ya está registrado en el sistema."
+    )
+    @ApiResponses(
+            value = {
+                @ApiResponse(
+                        responseCode = "200",
+                        description = "El email existe"
+                ),
+                @ApiResponse(
+                        responseCode = "404",
+                        description = "El email no existe"
+                )
+            }
+    )
     public ResponseEntity<Boolean> existsByEmail(@PathVariable(name = "email") String email) {
         boolean existe = userService.existsByEmail(email);
         if(existe) return ResponseEntity.ok(existe);
@@ -80,12 +134,24 @@ public class AuthController {
                 .build();
     }
 
-    @Operation(summary = "Verificar existencia por Username", description = "Comprueba si un nombre de usuario ya está en uso.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "El username existe"),
-            @ApiResponse(responseCode = "404", description = "El username no existe")
-    })
     @GetMapping("/exists-by-username/{username}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'AGENT')")
+    @Operation(
+            summary = "Verificar existencia por Username",
+            description = "Comprueba si un nombre de usuario ya está en uso."
+    )
+    @ApiResponses(
+            value = {
+                @ApiResponse(
+                        responseCode = "200",
+                        description = "El username existe"
+                ),
+                @ApiResponse(
+                        responseCode = "404",
+                        description = "El username no existe"
+                )
+            }
+    )
     public ResponseEntity<Boolean> existsByUsername(@PathVariable(name = "username") String username) {
         boolean existe = userService.existsByUsername(username);
 
@@ -96,12 +162,24 @@ public class AuthController {
                 .build();
     }
 
-    @Operation(summary = "Eliminar usuario", description = "Elimina físicamente un usuario de la base de datos por su ID.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Usuario eliminado correctamente"),
-            @ApiResponse(responseCode = "404", description = "No se encontró el usuario para eliminar")
-    })
     @DeleteMapping("/delete-by-id/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'AGENT', 'CLIENT', 'OWNER')")
+    @Operation(
+            summary = "Eliminar usuario",
+            description = "Elimina físicamente un usuario de la base de datos por su ID."
+    )
+    @ApiResponses(
+            value = {
+                @ApiResponse(
+                        responseCode = "200",
+                        description = "Usuario eliminado correctamente"
+                ),
+                @ApiResponse(
+                        responseCode = "404",
+                        description = "No se encontró el usuario para eliminar"
+                )
+            }
+    )
     public ResponseEntity<Void> deleteById(@PathVariable UUID id) {
         boolean eliminado = userService.deleteById(id);
 
@@ -116,26 +194,50 @@ public class AuthController {
         }
     }
 
-    @Operation(summary = "Iniciar Sesión", description = "Verifica credenciales (email y password) y devuelve el token generado.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Login correcto",
-                    content = @Content(schema = @Schema(implementation = Map.class))),
-            @ApiResponse(responseCode = "500", description = "Credenciales incorrectas o error interno", content = @Content)
-    })
     @PostMapping("/login")
+    @Operation(
+            summary = "Iniciar Sesión",
+            description = "Verifica credenciales (email y password) y devuelve el token generado."
+    )
+    @ApiResponses(
+            value = {
+                @ApiResponse(
+                        responseCode = "200",
+                        description = "Login correcto",
+                        content = @Content(
+                                schema = @Schema(implementation = Map.class)
+                        )
+                ),
+                @ApiResponse(
+                        responseCode = "500",
+                        description = "Credenciales incorrectas o error interno",
+                        content = @Content
+                )
+            }
+    )
     public ResponseEntity<AuthResponseDto> login(@RequestBody LoginDto loginDto) {
         return ResponseEntity.ok(userService.login(loginDto.email(), loginDto.password()));
     }
 
-    @Operation(summary = "Refrescar Token de Acceso", description = "Genera un nuevo JWT usando un Refresh Token válido.")
     @PostMapping("/refresh")
+    @Operation(
+            summary = "Refrescar Token de Acceso",
+            description = "Genera un nuevo JWT usando un Refresh Token válido."
+    )
     public ResponseEntity<AuthResponseDto> refreshToken(@Valid @RequestBody RefreshTokenRequestDto requestDto) {
         return ResponseEntity.ok(userService.refreshToken(requestDto.refreshToken()));
     }
 
-    @Operation(summary = "Listar usuarios por Rol", description = "Filtra y devuelve usuarios que tengan un rol específico (ADMIN, AGENT, OWNER, CLIENT).")
-    @ApiResponse(responseCode = "200", description = "Listado obtenido (puede estar vacío)")
     @GetMapping("/role/{userRole}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'AGENT')")
+    @Operation(
+            summary = "Listar usuarios por Rol",
+            description = "Filtra y devuelve usuarios que tengan un rol específico (ADMIN, AGENT, OWNER, CLIENT)."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Listado obtenido (puede estar vacío)"
+    )
     public ResponseEntity<List<UserDto>> getByRole(@PathVariable String userRole) {
         List<UserDto> users = userService.getByRole(userRole);
 
