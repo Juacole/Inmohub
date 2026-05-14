44933377d9a0783cddf712a2f1361583379b4927
Juacole <jvacotunjar@gmail.com>
2026-05-14 17:14:21 +0200

    fix: cambiar a listOf

--- inmohub-frontend/composeApp/src/commonMain/kotlin/com/inmohub/frontend/features/property/dtos/PropertyDto.kt
+++ inmohub-frontend/composeApp/src/commonMain/kotlin/com/inmohub/frontend/features/property/dtos/PropertyDto.kt
@@ -1,6 +1,5 @@
 package com.inmohub.frontend.features.property.dtos
 
-import androidx.datastore.preferences.protobuf.LazyStringArrayList.emptyList
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -15,5 +14,5 @@ data class PropertyDto(
     val status: String,
     val ownerId: String,
     val photos: List<String> = emptyList(),
-    val features: List<PropertyFeatureDto> = emptyList()
+    val features: List<PropertyFeatureDto> = listOf()
 )
