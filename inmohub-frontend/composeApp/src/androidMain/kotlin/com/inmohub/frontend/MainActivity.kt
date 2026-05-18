24271687370f6a0b12f95c4c36ee838a1e526fcf
Juacole <jvacotunjar@gmail.com>
2026-05-19 00:41:51 +0200

    fix: inicializar applicationContext en MainActivity para android

--- inmohub-frontend/composeApp/src/androidMain/kotlin/com/inmohub/frontend/MainActivity.kt
+++ inmohub-frontend/composeApp/src/androidMain/kotlin/com/inmohub/frontend/MainActivity.kt
@@ -6,11 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.inmohub.frontend.features.auth.data.local.applicationContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+
+        com.inmohub.frontend.features.auth.data.local.applicationContext = this
 
         setContent {
             App()
